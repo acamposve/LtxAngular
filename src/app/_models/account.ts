ce08331c14bbda121dee3aa1f5f2726845f0c962
Alex Campos <acampos@bamboopayment.com>
2021-06-05 14:34:44 -0300

    Commit

---
+++ src/app/_models/account.ts
@@ -0,0 +1,18 @@
+﻿import { Role } from './role';
+
+export class Account {
+    id: string;
+    title: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    role: Role;
+    jwtToken?: string;
+    identificationNumber: string;
+    address: string;
+    sector: string;
+    state: string;
+    city: string;
+    phone: string;
+
+}
---
+++ src/app/account/register.component.ts
@@ -0,0 +1,88 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+import { AccountService, AlertService } from '@app/_services';
+import { MustMatch } from '@app/_helpers';
+
+import swal from'sweetalert2';
+import Swal from 'sweetalert2';
+
+
+@Component({ templateUrl: 'register.component.html' })
+export class RegisterComponent implements OnInit {
+    form: FormGroup;
+    loading = false;
+    submitted = false;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private accountService: AccountService,
+        private alertService: AlertService
+    ) { }
+
+    ngOnInit() {
+        this.form = this.formBuilder.group({
+            title: ['', Validators.required],
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            identificationNumber: ['', Validators.required],
+            address: ['', Validators.required],
+            sector: ['', Validators.required],
+            state: ['', Validators.required],
+            city: ['', Validators.required],
+            phone: ['', Validators.required],
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required, Validators.minLength(6)]],
+            confirmPassword: ['', Validators.required],
+            acceptTerms: [false, Validators.requiredTrue]
+        }, {
+            validator: MustMatch('password', 'confirmPassword')
+        });
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.form.controls; }
+    erroalert(error: any)  
+    {  
+      Swal.fire({  
+        icon: 'error',  
+        title: 'Oops...',  
+        text: error,  
+        footer: '<a href>Why do I have this issue?</a>'  
+      })  
+    } 
+    onSubmit() {
+
+
+        console.log(this.form.value);
+        this.submitted = true;
+
+        // reset alerts on submit
+        this.alertService.clear();
+
+        // stop here if form is invalid
+        if (this.form.invalid) {
+
+            return;
+        }
+
+        this.loading = true;
+        this.accountService.register(this.form.value)
+            .pipe(first())
+            .subscribe({
+                next: () => {
+                    this.alertService.success('Registration successful, please check your email for verification instructions', { keepAfterRouteChange: true });
+                    this.router.navigate(['../login'], { relativeTo: this.route });
+                },
+                error: error => {
+                    this.erroalert(error)
+                    this.alertService.error(error);
+                    this.loading = false;
+                }
+            });
+    }
+}
